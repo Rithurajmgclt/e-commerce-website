2147517043ffaa26fe76529a4b96ac17160bada9
RITHURAJMG <rithurajmg2015@gmail.com>
2020-09-23 08:35:12 +0530

    uptodetailview

--- ecomsite/ecomsite/urls.py
+++ ecomsite/ecomsite/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from shop import views as shopview
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',shopview.index, name='index'),
+    path('<int:id>',shopview.detail,name='detail')
 ]
